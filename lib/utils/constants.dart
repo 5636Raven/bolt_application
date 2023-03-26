5751f995d5a69196d9fc535551ec02dd32d3b803
5636raven <raven.lopez01@utrgv.edu>
2023-03-26 12:26:57 -0500

    screens are added

---
+++ lib/utils/constants.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+//Color Palette
+const kThemeOffWhiteColor = Color(0xFFFEFAF6);
+const kThemePrimaryColor = Color.fromRGBO(139, 185, 231, 0); //pastel blue
+const kThemeSecondaryColor = Color.fromARGB(110, 93, 171, 223);
+const kThemeTertiaryColor = Color.fromARGB(255, 6, 66, 131);
+const kThemeDarkGrayColor = Color(0xFF404040);
+
+//Text Style
+const kSplashLogoTextStyle = TextStyle(
+  fontSize: 48,
+  fontFamily: "Poppins",
+  color: kThemePrimaryColor,
+  fontWeight: FontWeight.w600,
+);
+
+const kGetStartedLogoTextStyle = TextStyle(
+  fontSize: 36,
+  fontFamily: "Poppins",
+  color: kThemePrimaryColor,
+  fontWeight: FontWeight.w600,
+);
+
+const kGetStartedTaglineTextStyle = TextStyle(
+  fontSize: 16,
+  fontFamily: "Poppins",
+  color: kThemePrimaryColor,
+);
+
+const kGetStartedDescriptionText = TextStyle(
+  fontSize: 16,
+  fontFamily: "Montserrat",
+  color: kThemePrimaryColor,
+);
+
+const kGetStartedButtonText = TextStyle(
+  fontSize: 24,
+  fontFamily: "Poppins",
+  color: Colors.white,
+  fontWeight: FontWeight.w600,
+);
+
+const kDashboardLogoTextStyle = TextStyle(
+  fontSize: 16,
+  fontFamily: "Poppins",
+  color: kThemePrimaryColor,
+  fontWeight: FontWeight.w600,
+);
+
+const kDashboardHeadingTextStyle = TextStyle(
+  fontSize: 26,
+  fontFamily: "Poppins",
+  color: kThemePrimaryColor,
+  fontWeight: FontWeight.w600,
+);
+
+const kMetricsHeadingTextStyle = TextStyle(
+  fontSize: 12,
+  fontFamily: "Montserrat",
+  color: kThemePrimaryColor,
+);
+
+const kMetricsValueTextStyle = TextStyle(
+  fontSize: 36,
+  fontFamily: "Poppins",
+  color: kThemePrimaryColor,
+  fontWeight: FontWeight.w500,
+);
+
+const kMetricsValueSecondaryTextStyle = TextStyle(
+  fontSize: 24,
+  fontFamily: "Poppins",
+  color: kThemePrimaryColor,
+  fontWeight: FontWeight.w500,
+);
+
+const kDashboardTimeBodyText = TextStyle(
+  fontSize: 12,
+  fontFamily: "Poppins",
+  color: kThemePrimaryColor,
+);
+
+const kDashboardTimeHeadingText = TextStyle(
+  fontSize: 16,
+  fontFamily: "Poppins",
+  color: kThemePrimaryColor,
+  fontWeight: FontWeight.w500,
+);
+
+const kMetricLineChartHeadingText = TextStyle(
+  fontSize: 14,
+  fontFamily: "Poppins",
+  color: kThemePrimaryColor,
+  fontWeight: FontWeight.w400,
+);
+
+const kLogInScreenHeadingText = TextStyle(
+  fontSize: 26,
+  fontFamily: "Poppins",
+  color: kThemeTertiaryColor,
+  fontWeight: FontWeight.w600,
+);
+
+const kLogInScreenSubheadingText = TextStyle(
+  fontSize: 14,
+  fontFamily: "Poppins",
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+);
+
+const kLogInScreenForgotPasswordText = TextStyle(
+  fontSize: 16,
+  fontFamily: "Poppins",
+  color: kThemeTertiaryColor,
+  fontWeight: FontWeight.w500,
+);
+
+const kLogInScreenCreateAccountText = TextStyle(
+  fontSize: 16,
+  fontFamily: "Poppins",
+  color: kThemeTertiaryColor,
+  fontWeight: FontWeight.w500,
+  decoration: TextDecoration.underline,
+);
+
+const kUserProfileInfoText = TextStyle(
+  fontSize: 14,
+  fontFamily: "Poppins",
+  color: kThemeDarkGrayColor,
+);
+
+const kUserProfileButtonText = TextStyle(
+  fontSize: 14,
+  fontFamily: "Poppins",
+  fontWeight: FontWeight.w500,
+  color: kThemeTertiaryColor,
+);
+
+const kChangePasswordText = TextStyle(
+  fontSize: 14,
+  fontFamily: "Poppins",
+  color: kThemeDarkGrayColor,
+);
